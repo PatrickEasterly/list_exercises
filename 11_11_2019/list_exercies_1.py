df04a0041b1dbbfac75788a1998d9630fc1eb13c
Patrick <pateast678@gmail.com>
2019-11-11 14:17:26 -0500

    list_exercises again

--- 11_11_2019/list_exercies_1.py
+++ 11_11_2019/list_exercies_1.py
@@ -13,7 +13,34 @@
 # my_list = [1, 2, 3, 5, 2, 9, 1]
 # print(min(my_list))
 
-my_list = [1, 2, 3, 5, 2, 9, 1]
+# sort the numbers in a list
+# my_list = [5, 6, 2, 1, 9]
+# my_list.sort()
+# print(my_list)
 
-sorted = my_list.sort()
-print(sorted)
+# given a ist of numbers, print each number in the list that is even
+
+# my_list = [5, 6, 2, 1, 9, 8]
+
+# for x in my_list:
+#     if x % 2 == 0:
+#         print(x)
+
+# given a list of numbers, print the positive numbers
+
+# my_list = [5, 6, 2, 1, 9, -15, -23]
+# for x in my_list:
+#     if x > 0:
+#         print(x)
+#     else: 
+#         print('too small')
+
+# given a list of numbers, put the positive numbers in a new list
+
+# my_list = [5, 6, 2, 1, 9, -15, -23]
+# new_list = []
+# for x in my_list:
+#     if x > 0:
+#         new_list.append(x)
+# print(new_list)
+
