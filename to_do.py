5cf9a2a5c2fac69ee1631ceecaaa1ca05719fe94
Patrick <pateast678@gmail.com>
2019-11-13 14:46:26 -0500

    added delete todo

--- to_do.py
+++ to_do.py
@@ -1,3 +1,19 @@
-tooch = ['a', 'c', 'a', 'b']
-del tooch[2]
-print(tooch)
+#keep track of todos
+#add todos
+#delete todos
+#print todos
+#show main menu
+
+#add todos
+
+todo_list = []
+def add_todos(todo): 
+    todo_list.append(todo)
+
+#delete todos
+def delete_todo(todo):
+    del todo_list(todo)
+
+todo_list.append('f')
+delete_todo('f')
+print(todo_list)
