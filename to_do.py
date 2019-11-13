3f95f8ac89aa1681532ed141483f9bf93173a41b
Patrick <pateast678@gmail.com>
2019-11-13 15:47:59 -0500

    added main function to todo, added case for printing empty todo list

--- to_do.py
+++ to_do.py
@@ -4,16 +4,59 @@
 #print todos
 #show main menu
 
+    ###
+
+#keep track of todos
+todo_list = []
+
+#print todos
+
+def print_todos():
+    # print('\n')
+    if todo_list == []:
+        print('You have nothing to do. ')
+    for todo in todo_list:
+        print(todo)
 #add todos
 
-todo_list = []
-def add_todos(todo): 
+def add_todo(todo): 
     todo_list.append(todo)
 
-#delete todos
-def delete_todo(todo):
-    del todo_list(todo)
 
-todo_list.append('f')
-delete_todo('f')
-print(todo_list)
+#delete todos
+
+def delete_todo(index):
+    # del todo_list[index]
+    try:
+        todo_list.pop(index)
+    except IndexError:
+        print('Sorry, no todo at that index.')
+        
+
+
+def main():
+    menu = '''
+    Your favorite todo app
+    ======================
+    0. Quit
+    1. Print Todos
+    2. Add a Todo
+    3. Complete a todo
+    '''
+    is_running = True
+    while is_running == True:
+        print(menu)
+        choice = input('Choose one: ')
+        if choice == '0':
+            is_running = False
+        elif choice == '1':
+            print_todos()
+        elif choice == '2':
+            todo = input('Your todo: ')
+            add_todo(todo)
+        elif choice == '3':
+            pass
+        else:
+            print('\nPlease enter a valid choice')
+
+main()
